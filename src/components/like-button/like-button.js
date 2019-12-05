c4e69cbb01f498a4d1a28f64481caf848b11f2a0
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2019-12-05 16:00:21 +0300

    Finished js like-button

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -8,12 +8,21 @@ $(function(){
         if ($(this).hasClass('like-button-on')) {
             $(this).trigger('on.likeButton');
 
-            let e = $(this).children('like-button__like');
-            console.log(e);
+            $(this).find($('.like-button__like')).text('favorite');
+            let count = $(this).find($('.like-button__count')).text();
+            count = parseInt(count);
+            $(this).find($('.like-button__count')).text(++count);
+
         } else {
             $(this).trigger('off.likeButton');
+
+            $(this).find($('.like-button__like')).text('favorite_border');
+            let count = $(this).find($('.like-button__count')).text();
+            count = parseInt(count);
+            $(this).find($('.like-button__count')).text(--count);
         }
     });
+
 
     $('like-button').on('on.likeButton', function () {
         $(this).addClass('like-button-on');
