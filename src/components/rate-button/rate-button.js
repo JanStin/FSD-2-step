cb06fdec65a85ef19cf4a6cbb3a7e37f3959e559
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2019-12-06 16:16:54 +0300

    Refactored code input-gender.sass

--- src/components/rate-button/rate-button.js
+++ src/components/rate-button/rate-button.js
@@ -3,7 +3,7 @@ import './rate-button.sass'
 $(document).ready(function(){
     $('.rate-button .rate-button__star').on('mouseover', function(){
         // Получаем номер data-value и переводим в числовое значение.
-        var onStar = parseInt($(this).data('value'), 10); 
+        let onStar = parseInt($(this).data('value'), 10); 
 
         // Заменяем 'star' до выбранного элемента включительно, и на 'star_border' после через родителя.
         $(this).parent().children('.rate-button__star').each(function(e){
