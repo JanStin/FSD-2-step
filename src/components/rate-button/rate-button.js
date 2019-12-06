139ceb537bce6fdbe25e0ae5f9e66041c2984a27
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2019-12-06 14:03:20 +0300

    Finished rate-button js and sass file

--- src/components/rate-button/rate-button.js
+++ src/components/rate-button/rate-button.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
     $('.rate-button .rate-button__star').on('mouseover', function(){
         // Получаем номер data-value и переводим в числовое значение.
         var onStar = parseInt($(this).data('value'), 10); 
-        console.log(onStar);
 
         // Заменяем 'star' до выбранного элемента включительно, и на 'star_border' после через родителя.
         $(this).parent().children('.rate-button__star').each(function(e){
